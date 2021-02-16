1685d868c5805201c152fe5d1a0f1422a3b64fb8
Mahdeen (Neo) Khan <mahdeen.s.khan@yale.edu>
2021-02-16 00:39:12 -0500

    made pathing with svg

--- tour/main.js
+++ tour/main.js
@@ -1,7 +1,11 @@
 const nodes = document.querySelectorAll('.node');
 const modal = document.querySelector('.modal');
-const originalImg = document.querySelector('.full-img');
+const origImg = document.querySelector('.full-img');
+const map = document.querySelector('#map');
+const svgNS = map.namespaceURI;
 const padding = 20;
+
+let nodesClicked = [];
 
 nodes.forEach(node => {
   let xPos = Math.random() * (window.innerWidth - padding);
@@ -12,10 +16,12 @@ nodes.forEach(node => {
 
   node.addEventListener('click', event => {
     modal.classList.add("modal-open");
-    originalImg.classList.add("img-open");
+    origImg.classList.add("img-open");
+
+    nodesClicked = nodesClicked.concat({xPos, yPos});
     
     const id = event.target.id;
-    originalImg.src = `assets/pic${id}.jpg`;
+    origImg.src = `assets/pic${id}.jpg`;
   });
 });
 
@@ -23,6 +29,17 @@ nodes.forEach(node => {
 modal.addEventListener('click', (event) => {
   if (event.target.classList.contains('modal')) {
       modal.classList.remove("modal-open");
-      originalImg.classList.remove("img-open");
+      origImg.classList.remove("img-open");
+  }
+  if (nodesClicked.length >= 2) {
+    const line = document.createElementNS(svgNS,'line');
+    line.setAttribute('x1', nodesClicked[nodesClicked.length - 2].xPos + 5);
+    line.setAttribute('y1', nodesClicked[nodesClicked.length - 2].yPos + 5);
+    line.setAttribute('x2', nodesClicked[nodesClicked.length - 1].xPos + 5);
+    line.setAttribute('y2', nodesClicked[nodesClicked.length - 1].yPos + 5);
+    line.setAttribute('stroke', 'black');
+    // line.setAttribute('width', 2);
+
+    map.append(line);
   }
 });
