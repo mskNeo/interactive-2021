91dadf0d63349881ffea20f01a585e68237d5405
Mahdeen (Neo) Khan <mahdeen.s.khan@yale.edu>
2021-04-24 21:59:46 -0400

    updated site with links

---
+++ exercises/window/script.js
@@ -0,0 +1,9 @@
+const time = new Date();
+const hrCloud = document.querySelector('.cloud1');
+const minCloud = document.querySelector('.cloud2');
+
+const hour = time.getHours();
+const min = time.getMinutes();
+
+console.log(hour);
+console.log(min);
